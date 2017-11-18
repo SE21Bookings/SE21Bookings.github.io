844611c43b8065461051a55288ddc0b1c34cd93c
shenyicui <shenyicui@outlook.com>
2017-11-18 23:15:50 +0800

    bug fix

--- JS/MAIN.js
+++ JS/MAIN.js
@@ -9,7 +9,7 @@ var getFunc = "msgRetrieve";
 var CombindedBookTimeTech1 ="";
 function combineString()
 {
-    CombindedBookTimeTech1 = "<hr>TECH 1:<hr>" + "Name:" + document.getElementById("name").value + "<hr>Email:" + document.getElementById("email").value + "<hr>Time:" + document.getElementById("PickPeriod").value+"<hr>"
+    CombindedBookTimeTech1 = "<hr>TECH 1:<hr>" + "name:" + document.getElementById("name").value + "<hr>Email:" + document.getElementById("email").value + "<hr>Time:" + document.getElementById("PickPeriod").value+"<hr>"
     setValue();
 	
 }
