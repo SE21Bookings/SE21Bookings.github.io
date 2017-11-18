15dbc2a5407e2838a5b392d12e86fd4802ea76c4
shenyicui <shenyicui@outlook.com>
2017-11-18 22:32:51 +0800

    first Launch

---
+++ JS/MAIN.js
@@ -0,0 +1,36 @@
+      
+var deviceID    = "470041001051363036373538";
+var accessToken = "a9d844e915fa5183963dfa166907e08d0cd946f9";
+
+var setFunc = "SendText";
+    
+var getFunc = "msgRetrieve";
+
+var CombindedBookTimeTech1 ="";
+function combineString()
+{
+    CombindedBookTimeTech1 = "<hr>TECH 1: <hr>Booked By:" + document.getElementById("email").value + "<hr>Email:" + document.getElementById("name").value + "<hr>Time:" + document.getElementById("PickPeriod").value+"<hr>"
+    setValue();
+	
+}
+
+function setValue(obj) {
+var newValue = CombindedBookTimeTech1;
+sparkSetPos(newValue);
+}
+      
+
+function sparkSetPos(newValue) {
+    var requestURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + setFunc + "/";
+    $.post( requestURL, { params: newValue, access_token: accessToken });
+}
+        
+window.setInterval(function() {
+        requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + getFunc + "/?access_token=" + accessToken;
+        $.getJSON(requestURL, function(json) {
+                 document.getElementById("currentBook").innerHTML = json.result;
+                 });
+      }, 1000);
+        
+
+
