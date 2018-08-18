511f2cff1c03f760be05cba4549257ec63072f3f
Shenyi Cui <shenyicui@outlook.com>
2018-08-18 18:08:36 +0800

    Upload Code Fix

--- JS/UploadCode.js
+++ JS/UploadCode.js
@@ -1,19 +1,30 @@
 function upload()
 {
-	$.ajax({
-		url:"file:///Users/shenyi/Desktop/SE21Tech1.csv",
-		dataType:"text",
-		success:function(data)
-		{
-			var timetable_data = data.split(/\r?\n|\r/);
+	loadFileAsText()
+	function loadFileAsText()
+	{
+      var fileToLoad = document.getElementById("fileToLoad").files[0];
+
+	  var fileReader = new FileReader();
+	  fileReader.onload = function(fileLoadedEvent){
+		  var textFromFileLoaded = fileLoadedEvent.target.result;
+		  var timetable_data = textFromFileLoaded.split(/\r?\n|\r/);
 			for(var count = 0; count<timetable_data.length; count++)
 			{
 				var cell_data = timetable_data[count].split(",");
 				for(var cell_count=0; cell_count<cell_data.length; cell_count++)
 				{
-					console.log(cell_data[cell_count]);
+					if(count != 0)
+					{
+						console.log(cell_data[cell_count]);
+					}
+					
 				}
 			}
-		}
-	});
+	  };
+
+	  fileReader.readAsText(fileToLoad, "UTF-8");
+	}
+	
+	
 }
